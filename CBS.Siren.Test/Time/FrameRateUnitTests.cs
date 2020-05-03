8d8f0e6fd97fb6c9a56d7b69a4c82ed11767745c
Edmund Lewry <edmund.lewry@ntlworld.com>
2020-05-03 20:56:52 +0100

    Implemented unit tests for Frame Rate IsDropFrame call

---
+++ CBS.Siren.Test/Time/FrameRateUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using CBS.Siren.Time;
+using Xunit;
+
+namespace CBS.Siren.Test.Time
+{
+    public class FrameRateUnitTests
+    {
+        [Theory]
+        [Trait("TestType", "UnitTest")]
+        [InlineData(FrameRate.DF30)]
+        public void IsDropFrame_GivenADropDrameFrameRate_ShouldReportTrue(FrameRate frameRate)
+        {
+            Assert.True(frameRate.IsDropFrame());
+        }
+        
+        [Theory]
+        [Trait("TestType", "UnitTest")]
+        [InlineData(FrameRate.FPS24)]
+        [InlineData(FrameRate.FPS25)]
+        [InlineData(FrameRate.FPS30)]
+        public void IsDropFrame_GivenANonDropDrameFrameRate_ShouldReportFalse(FrameRate frameRate)
+        {
+            Assert.False(frameRate.IsDropFrame());
+        }
+    }
+}
