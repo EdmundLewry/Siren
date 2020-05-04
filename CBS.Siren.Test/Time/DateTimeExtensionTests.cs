16348dff39803b3f0ed60bb252c881822711feeb
Edmund Lewry <edmund.lewry@ntlworld.com>
2020-05-04 20:37:39 +0100

    Implemented functionality and tests for DateTime from timecode string

--- CBS.Siren.Test/Time/DateTimeExtensionTests.cs
+++ CBS.Siren.Test/Time/DateTimeExtensionTests.cs
@@ -1,10 +1,41 @@
 ﻿using CBS.Siren.Time;
+using System;
 using Xunit;
 
 namespace CBS.Siren.Test.Time
 {
     public class DateTimeExtensionTests
     {
-        
+        [Theory]
+        [Trait("TestType", "UnitTest")]
+        [InlineData("2020-03-22T00:00:00:00", FrameRate.FPS25, "22/03/2020 00:00:00.000")]
+        [InlineData("2020-03-22T00:00:10:05", FrameRate.FPS25, "22/03/2020 00:00:10.200")]
+        [InlineData("2020-03-22T00:00:10:05", FrameRate.FPS30, "22/03/2020 00:00:10.167")]
+        [InlineData("2020-03-22T12:30:10;05", FrameRate.DF30, "22/03/2020 12:30:10.167")]
+        [InlineData("1980-12-15T22:20:45:05", FrameRate.FPS24, "15/12/1980 22:20:45.208")]
+        [InlineData("2020-03-22", FrameRate.FPS25, "22/03/2020 00:00:00.000")]
+        public void FromTimecodeString_GivenStringAndFramerate_ShouldReturnDateTime(string input, FrameRate frameRate, string expectedOutput)
+        {
+            DateTime valueUnderTest = DateTimeExtensions.FromTimecodeString(input, frameRate);
+            DateTime expectedValue = DateTime.Parse(expectedOutput);
+            Assert.Equal(expectedValue, valueUnderTest);
+        }
+
+        [Theory]
+        [Trait("TestType", "UnitTest")]
+        [InlineData("2020-03-38T00:00:00:00", FrameRate.FPS25)]
+        [InlineData("2020-13-22T00:00:10:05", FrameRate.FPS30)]
+        [InlineData("2020-03-22T12:30:10;40", FrameRate.DF30)]
+        [InlineData("1980-12-15T25:20:45:05", FrameRate.FPS24)]
+        [InlineData("1980-12-15T20:72:45:05", FrameRate.FPS24)]
+        [InlineData("1980-12-15T20:20:75:05", FrameRate.FPS24)]
+        [InlineData("2020-03-28T00:00:00:000", FrameRate.FPS25)]
+        [InlineData("22020-03-28T05:10:00;00", FrameRate.DF30)]
+        [InlineData("2020-03-28T00:00:00:00", FrameRate.DF30)]
+        [InlineData("2020-03-22T", FrameRate.FPS25)]
+        public void FromTimecodeString_GivenAnInvalidString_ShouldThrow(string input, FrameRate frameRate)
+        {
+            Assert.Throws<ArgumentException>(() => DateTimeExtensions.FromTimecodeString(input, frameRate));
+        }
     }
 }
--- CBS.Siren/Time/DateTimeExtensions.cs
+++ CBS.Siren/Time/DateTimeExtensions.cs
@@ -1,18 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CBS.Siren.Time
 {
     public static class DateTimeExtensions
     {
+        private const string TIMECODE_MATCH = @"(?<hour>[0-1][0-9]|2[0-3]):(?<minute>[0-5][0-9]):(?<second>[0-5][0-9])(?<seperator>[:;])(?<frame>[0-1][0-9]|2[0-9])";
+        private const string DATE_MATCH = @"(?<year>[0-9]{4})-(?<month>0[0-9]|1[0-2])-(?<day>[0-2][0-9]|3[01])";
+        private const string DATE_REGEX = "\\b"+ DATE_MATCH + "\\b";
+        private const string DATETIME_REGEX = "\\b" + DATE_MATCH + "T" + TIMECODE_MATCH + "\\b";
+
         public static DateTime FromTimecodeString(string timecode) => FromTimecodeString(timecode, TimeSource.SourceFrameRate);
         public static DateTime FromTimecodeString(string timecode, FrameRate frameRate)
         {
-            //Validate string
-            //Convert string
-            //Build DT
-            return DateTime.Now;
+            Regex timeRegex = new Regex(DATETIME_REGEX);
+            Match match = timeRegex.Match(timecode);
+
+            if(!match.Success)
+            {
+                timeRegex = new Regex(DATE_REGEX);
+                match = timeRegex.Match(timecode);
+            }
+
+            ValidateTimeString(match, timecode, frameRate);
+
+            int year = int.Parse(match.Groups["year"].Value);
+            int month = int.Parse(match.Groups["month"].Value);
+            int day = int.Parse(match.Groups["day"].Value);
+            int hour = match.Groups["hour"].Success ? int.Parse(match.Groups["hour"].Value) : 0;
+            int minute = match.Groups["minute"].Success ? int.Parse(match.Groups["minute"].Value) : 0;
+            int second = match.Groups["second"].Success ? int.Parse(match.Groups["second"].Value) : 0;
+            int millisecond = match.Groups["frame"].Success ? TimeUtilities.FramesToMiliseconds(int.Parse(match.Groups["frame"].Value), frameRate) : 0;
+
+            return new DateTime(year, month, day, hour, minute, second, millisecond);
+        }
+
+        private static void ValidateTimeString(Match match, string timecode, FrameRate frameRate)
+        {
+            if(!match.Success)
+            {
+                throw new ArgumentException($"Timecode (Value: {timecode}) string does not match accepted format", "timecode");
+            }
+
+            Group seperator = match.Groups["seperator"];
+            if (!seperator.Success)
+            {
+                return;
+            }
+
+            if (frameRate.IsDropFrame())
+            {
+                if (!seperator.Value.Equals(";"))
+                    throw new ArgumentException($"Timecode (Value: {timecode}) string was expected to be dropframe, but does not include DF seperator", "timecode");
+            }
+            else
+            {
+                if (!seperator.Value.Equals(":"))
+                    throw new ArgumentException($"Timecode (Value: {timecode}) string was expected not to be dropframe, but includes the DF seperator", "timecode");
+            }
+
+            int frames = int.Parse(match.Groups["frame"].Value);
+            if (frames >= frameRate.FrameCount())
+            {
+                throw new ArgumentException($"Timecode (Value: {timecode}) frames is greater than the frame rate {Enum.GetName(typeof(FrameRate), frameRate)}", "timecode");
+            }
         }
 
         public static string ToTimecodeString(this DateTime dateTime) => ToTimecodeString(dateTime, TimeSource.SourceFrameRate);
