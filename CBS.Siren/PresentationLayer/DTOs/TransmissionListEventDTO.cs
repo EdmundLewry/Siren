193282d9c0698ddd031aca56e33dc2a2d667bf2c
Edmund Lewry <edmund.lewry@ntlworld.com>
2020-05-25 07:33:51 +0100

    Added TransmissionListEventDTO to present TransmissionListEvent

---
+++ CBS.Siren/PresentationLayer/DTOs/TransmissionListEventDTO.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CBS.Siren.DTO
+{
+    public class TransmissionListEventDTO
+    {
+        public string EventState { get; set; }
+        public Guid Id { get; set; }
+        public string EventTimingStrategy { get; set; }
+        public int EventFeatureCount { get; set; }
+        public TimeSpan ExpectedDuration { get; set; }
+        public DateTime ExpectedStartTime { get; set; }
+        public string RelatedPlaylistEvent { get; set; }
+        public int RelatedDeviceListEventCount { get; private set; }
+    }
+}
