afbae6804fcbeb5346664200d758b8437df1acdd
Edmund Lewry <edmund.lewry@ntlworld.com>
2020-07-26 11:25:51 +0100

    Changed ClientApp folder to frontend folder

--- CBS.SirenUI/Startup.cs
+++ CBS.SirenUI/Startup.cs
@@ -24,7 +24,7 @@ namespace CBS.SirenUI
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "Frontend/dist";//"ClientApp/dist";
             });
         }
 
@@ -63,7 +63,7 @@ namespace CBS.SirenUI
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "Frontend";
 
                 if (env.IsDevelopment())
                 {
