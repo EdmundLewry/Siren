0e0b5a481203eb58097e4910867659aa711d1218
Edmund Lewry <edmund.lewry@ntlworld.com>
2020-08-02 18:32:49 +0100

    Implemented basic intial buttons to the Transmission List Event UI. Needs some thought about the best way to do the player controls.

--- CBS.SirenUI/Frontend/projects/automation/src/lib/components/components.module.ts
+++ CBS.SirenUI/Frontend/projects/automation/src/lib/components/components.module.ts
@@ -4,6 +4,9 @@ import { RouterModule } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { TransmissionlistListComponent } from './transmissionlist-list/transmissionlist-list.component';
 import { TranmissionlistEventsListComponent } from './tranmissionlist-events-list/tranmissionlist-events-list.component';
@@ -17,7 +20,10 @@ import { TranmissionlistEventsListComponent } from './tranmissionlist-events-lis
     RouterModule,
     MatTableModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class ComponentsModule { }
