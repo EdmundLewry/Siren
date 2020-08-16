dfb561b96e56fd664414d3610557cb35d95eab12
Edmund Lewry <edmund.lewry@ntlworld.com>
2020-08-16 18:07:47 +0100

    Replaced Top Nav with side nav and removed the use of the Transmission Lists view. I'm going to replace it with a Channel View. I still need to tidy the styling for the side nav elements

--- CBS.SirenUI/Frontend/src/app/app.module.ts
+++ CBS.SirenUI/Frontend/src/app/app.module.ts
@@ -7,19 +7,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { SidenavMenuComponent } from './components/sidenav-menu/sidenav-menu.component';
 
 const routes: Routes = [
   { path: "automation", loadChildren: () => import('automation').then((library) => library.AutomationModule) },
-  { path: "", redirectTo: "automation/txlist", pathMatch: 'full' }
+  { path: "", redirectTo: "automation/txlist/1", pathMatch: 'full' }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    SidenavMenuComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,7 +33,10 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     MatToolbarModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
